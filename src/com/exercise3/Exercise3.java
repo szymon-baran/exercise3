44383fe3268e3ed1c7270307d339e856757ca2c3
Szymon Wojciech Baran <szymon.w.baran@gmail.com>
2021-01-27 15:53:20 +0100

    Poprawa bledu - zmieniono typ liczby e z double na BigDecimal.

--- src/com/exercise3/Exercise3.java
+++ src/com/exercise3/Exercise3.java
@@ -1,22 +1,28 @@
 package com.exercise3;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Exercise3 {
     //Wyliczenie silni.
-    static long getFactorial(int num) {
-        long factorial = 1;
+    static BigDecimal getFactorial(int num) {
+        BigDecimal factorial = new BigDecimal("1");
         for (int i=1; i<num+1; i++) {
-            factorial *= i;
+            factorial = factorial.multiply(BigDecimal.valueOf(i));
         }
         return factorial;
     }
 
     //Wyliczenie liczby e zgodnie ze wzorem z Wikipedii podanej w wiadomosci - poprzez sumę szeregu z n=15.
-    static double getE() {
-        double e=0, tmp;
-        for(int i=0; i<15; i++) {
-            tmp = 1.0/getFactorial(i);
-            e += tmp;
+    static BigDecimal getE() {
+        BigDecimal e = new BigDecimal("0.0");
+        BigDecimal tmp;
+        BigDecimal one = new BigDecimal("1.0");
+        for(int i=0; i<80; i++) {
+            tmp = one.divide(getFactorial(i), 120, RoundingMode.CEILING);
+            e = e.add(tmp);
         }
+        System.out.println(e);
         return e;
     }
 
@@ -36,8 +42,8 @@ public class Exercise3 {
     }
 
     public static void main(String[] args) {
-        double e = getE();
-        String str = String.valueOf(e);
+        BigDecimal e = getE();
+        String str = e.toPlainString();
         System.out.println("Wyznaczona liczba e: " + str);
         String mainStr = str.substring(2);
         System.out.println("Zbior cyfr do poszukiwan: " + mainStr);
